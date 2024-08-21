0c72edb97a1d120bbfacb134431ce40923578a8e
Julian Theis <theis@next-kraftwerke.de>
2024-08-21 16:08:37 +0200

    Add tests for query methods

--- tests/outflux_test.py
+++ tests/outflux_test.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from uuid import UUID
 
 from hypothesis import given, strategies as st
 
@@ -8,7 +9,6 @@ from outflux.outflux import Outflux, ZONE_INFO
 @given(db=st.text(), measurement=st.text(), start=st.datetimes(), end=st.datetimes())
 def test_init(db: str, measurement: str, start: datetime, end: datetime) -> None:
     url = "http://influxdb-unittest:8086"
-
     outflux = Outflux(url, db, measurement, start, end)
 
     assert outflux.url == url
@@ -17,3 +17,37 @@ def test_init(db: str, measurement: str, start: datetime, end: datetime) -> None
 
     assert datetime.fromisoformat(outflux.start) == start.astimezone(ZONE_INFO)
     assert datetime.fromisoformat(outflux.end) == end.astimezone(ZONE_INFO)
+
+
+@given(uuid=st.uuids(), start=st.datetimes(), end=st.datetimes())
+def test_query_select(uuid: UUID, start: datetime, end: datetime) -> None:
+    url = "http://influxdb-unittest:8086"
+    db = "testdb"
+    measurement = "unittests"
+    outflux = Outflux(url, db, measurement, start, end)
+    query = outflux.query_select(uuid)
+
+    assert query.lower().startswith("select")
+    assert measurement in query
+
+    assert f"'{start.astimezone(ZONE_INFO).isoformat()}'" in query
+    assert f"'{end.astimezone(ZONE_INFO).isoformat()}'" in query
+
+    assert f"'{uuid}'" in query
+
+
+@given(uuid=st.uuids(), start=st.datetimes(), end=st.datetimes())
+def test_query_delete(uuid: UUID, start: datetime, end: datetime) -> None:
+    url = "http://influxdb-unittest:8086"
+    db = "testdb"
+    measurement = "unittests"
+    outflux = Outflux(url, db, measurement, start, end)
+    query = outflux.query_delete(uuid)
+
+    assert query.lower().startswith("delete")
+    assert measurement in query
+
+    assert f"'{start.astimezone(ZONE_INFO).isoformat()}'" in query
+    assert f"'{end.astimezone(ZONE_INFO).isoformat()}'" in query
+
+    assert f"'{uuid}'" in query
